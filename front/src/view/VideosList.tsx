cb50dbeb4e7707aa4500f5153ca916a9e211303f
Mickael <mickael.ramaroson@antennereunion.fr>
2022-04-15 09:26:54 +0300

    fix request

--- front/src/view/VideosList.tsx
+++ front/src/view/VideosList.tsx
@@ -6,6 +6,12 @@ import VideoItem from "../components/video-item/VideoItem";
 import socket from "../utils/socket.utils";
 import VideoPlayer from "../components/video-player/VideoPlayer";
 import "../styles/videosList.scss";
+import useSWR from "swr";
+
+const fetchVideos = async (url: string) => {
+  const response = await axios.get(url);
+  return (response.data || []).filter((item: any) => item.name);
+};
 
 const VideosList: FC = () => {
   const [videos, setVideos] = useState<any[]>([]);
@@ -14,20 +20,20 @@ const VideosList: FC = () => {
     filename: "",
     name: "",
   });
+  const { data } = useSWR("/files", fetchVideos);
+
   // get videos from API
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get("/files");
-      setVideos((response.data || []).filter((item: any) => item.name));
-    };
-    fetchData();
-
+    if (data) {
+      setVideos(data);
+    }
     // update list when a video is uploaded anywhere
     socket &&
-      socket.on("update-list", () => {
-        fetchData();
+      socket.on("update-list", async () => {
+        const data = await fetchVideos("/files");
+        setVideos(data);
       });
-  }, []);
+  }, [data]);
 
   const handleClickVideoItem = (object: any) => {
     setVideoPlayerProps({
@@ -51,7 +57,7 @@ const VideosList: FC = () => {
           <h1 className="title">Voici les vidéos uploadées</h1>
         </Box>
         <Grid spacing={2} container>
-          {videos.map((video) => (
+          {(videos || []).map((video) => (
             <Grid key={video.filename} item xs={12} md={6} lg={6}>
               <VideoItem {...video} onClick={handleClickVideoItem} />
             </Grid>
